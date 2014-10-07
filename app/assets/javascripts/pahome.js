ac74d3f27b148d5c8ed806097562df4ce3ef2292
Eval Air <pkuimehyd@163.com>
2014-10-07 21:21:11 +0800

    7
    1. bootstrap style carsouel
    2. one-page scroll

--- app/assets/javascripts/pahome.js
+++ app/assets/javascripts/pahome.js
@@ -1,32 +1,12 @@
-//def owl function
-function owlCarousel () {
-    $("#intro").owlCarousel({
- 
-      navigation: false, // Show next and prev buttons
-      singleItem: true,
-      autoPlay: 3500,
-      transitionStyle: "fade",
-      lazyLoad : true,
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-  });
-};
-
-
 //def #intro full screen
 function fullScreenContainer() {
     var t = $(window).width() + "px", e = $(window).height() + "px";
-    $("#intro, #intro .item, #intro-video").css({
+    $("#carousel .item, .page_container").css({
         width: t,
         height: e
     }), $(window).resize(function() {
         var t = $(window).width() + "px", e = $(window).height() + "px";
-        $("#intro, #intro .item, #intro-video, #intro-video .item").css({
+        $("#carousel .item, .page_container").css({
             width: t,
             height: e
         })
@@ -41,13 +21,28 @@ $(window).load(function(){
 });
 
 
-//intro owl carousel
+
 $(document).ready(function() {
-  // set owl carousel
-  owlCarousel();
 
   //let intro full screen
   fullScreenContainer();
+
+  $(".main").onepage_scroll({
+   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+   keyboard: true,                  // You can activate the keyboard controls
+   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                    // the browser's width is less than 600, the fallback will kick in.
+   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+  });
  
 });
 
